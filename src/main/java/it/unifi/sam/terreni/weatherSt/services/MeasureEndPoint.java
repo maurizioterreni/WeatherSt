05cb9509f2c43d761d98eea3edb423db56bc1883
maurizioterreni <maurizio.terreni@live.it>
2018-09-16 18:53:06 +0200

    refactor meause end point

--- src/main/java/it/unifi/sam/terreni/weatherSt/services/MeasureEndPoint.java
+++ src/main/java/it/unifi/sam/terreni/weatherSt/services/MeasureEndPoint.java
@@ -1,35 +1,39 @@
 package it.unifi.sam.terreni.weatherSt.services;
 
+import static java.time.temporal.TemporalAdjusters.firstDayOfYear;
+import static java.time.temporal.TemporalAdjusters.lastDayOfYear;
+import static java.time.temporal.TemporalAdjusters.firstDayOfMonth;
+import static java.time.temporal.TemporalAdjusters.lastDayOfMonth;
+
+
+
 import java.time.Instant;
 import java.time.LocalDateTime;
 import java.time.ZoneId;
-import java.util.HashSet;
+import java.util.ArrayList;
 import java.util.List;
-import java.util.Set;
 
 import javax.inject.Inject;
 import javax.transaction.Transactional;
 import javax.ws.rs.Consumes;
 import javax.ws.rs.GET;
-import javax.ws.rs.HeaderParam;
 import javax.ws.rs.POST;
 import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
 
-import it.unifi.sam.terreni.weatherSt.dao.WeatherStationDao;
 import it.unifi.sam.terreni.weatherSt.dao.measure.MeasureDao;
 import it.unifi.sam.terreni.weatherSt.dao.measure.UnitMeasureKnowledgeDao;
 import it.unifi.sam.terreni.weatherSt.dao.sensor.SensorDao;
-import it.unifi.sam.terreni.weatherSt.dto.measure.MeasureChartDto;
+import it.unifi.sam.terreni.weatherSt.dto.measure.MeasureDto;
 import it.unifi.sam.terreni.weatherSt.dto.measure.MeasurePostRequestDto;
-import it.unifi.sam.terreni.weatherSt.model.WeatherStation;
 import it.unifi.sam.terreni.weatherSt.model.measure.Measure;
 import it.unifi.sam.terreni.weatherSt.model.measure.UnitMeasureKnowledge;
 import it.unifi.sam.terreni.weatherSt.model.sensor.Sensor;
 import it.unifi.sam.terreni.weatherSt.utils.ErrorServices;
-import it.unifi.sam.terreni.weatherSt.utils.GroupByClass;
+import it.unifi.sam.terreni.weatherSt.utils.StringUtils;
 
 
 
@@ -39,8 +43,6 @@ public class MeasureEndPoint {
 	private SensorDao sensorDao;
 	@Inject
 	private MeasureDao measureDao;
-	@Inject
-	private WeatherStationDao weatherStationDao;
 	@Inject
 	private UnitMeasureKnowledgeDao unitMeasureKnowledgeDao;
 
@@ -81,13 +83,11 @@ public class MeasureEndPoint {
 	
 
 	@GET
+	@Path("/sensor/{id}")
 	@Produces(MediaType.APPLICATION_JSON)
 	@Transactional
-	public Response get(@HeaderParam("sensorId") Long sensorId) {
-		if (sensorId == null)
-			return Response.status(Response.Status.INTERNAL_SERVER_ERROR).entity(ErrorServices.NULL_OBJECT.getMessage() + " - sensorId").build();
-
-		Sensor sensor = sensorDao.findById(sensorId);
+	public Response getBySensorId(@PathParam("id") Long id) {
+		Sensor sensor = sensorDao.findById(id);
 
 		if(sensor == null)
 			return Response.status(Response.Status.NOT_FOUND).entity(ErrorServices.OBJECT_NOT_FOUND.getMessage() + " - sensor").build();
@@ -101,80 +101,175 @@ public class MeasureEndPoint {
 	}
 	
 	@GET
-	@Path("/lastMeasure")
+	@Path("/sensor/{id}/getValueOfYear")
 	@Produces(MediaType.APPLICATION_JSON)
 	@Transactional
-	public Response getLastOfWeather(@HeaderParam("weatherId") Long weatherId) {
-		if (weatherId == null)
-			return Response.status(Response.Status.INTERNAL_SERVER_ERROR).entity(ErrorServices.NULL_OBJECT.getMessage() + " - sensorId").build();
-
-		WeatherStation weatherStation = weatherStationDao.fetchById(weatherId);
-
-		if(weatherStation == null)
-			return Response.status(Response.Status.NOT_FOUND).entity(ErrorServices.OBJECT_NOT_FOUND.getMessage() + " - weatherStation").build();
-		
-		if(weatherStation.getSensors() == null || weatherStation.getSensors().size() == 0)
-			return Response.status(Response.Status.NOT_FOUND).entity(ErrorServices.OBJECT_NOT_FOUND.getMessage() + " - sensor").build();
-
-		Set<Measure> measures = new HashSet<>();
-		
-		for (Sensor sensor : weatherStation.getSensors()) {
-			measures.add(measureDao.getLastMeasue(sensor));
-		}
-
-		return Response.status(200).entity(measures).build();
-	}
-	
-	
-	
-	@GET
-	@Path("/getMeasureByDate")
-	@Produces(MediaType.APPLICATION_JSON)
-	@Transactional
-	public Response getMeasureByDate(@HeaderParam("sensorId") Long sensorId, @HeaderParam("fromDate") Long fromDate, @HeaderParam("toDate") Long toDate) {
-		if (sensorId == null)
-			return Response.status(Response.Status.INTERNAL_SERVER_ERROR).entity(ErrorServices.NULL_OBJECT.getMessage() + " - sensorId").build();
-		if (fromDate == null)
-			return Response.status(Response.Status.INTERNAL_SERVER_ERROR).entity(ErrorServices.NULL_OBJECT.getMessage() + " - fromDate").build();
-		if (toDate == null)
-			return Response.status(Response.Status.INTERNAL_SERVER_ERROR).entity(ErrorServices.NULL_OBJECT.getMessage() + " - toDate").build();
-		
-		Sensor sensor = sensorDao.findById(sensorId);
+	public Response getValueOfYear(@PathParam("id") Long id) {
+		Sensor sensor = sensorDao.findById(id);
 		
 		if(sensor == null)
-			return Response.status(Response.Status.INTERNAL_SERVER_ERROR).entity(ErrorServices.NULL_OBJECT.getMessage() + " - sensor").build();
-
-		if(fromDate > toDate) {
-			Long temp = fromDate;
-			fromDate = toDate;
-			toDate = temp;
+			return Response.status(Response.Status.NOT_FOUND).entity(ErrorServices.OBJECT_NOT_FOUND.getMessage() + " - sensor").build();
+		
+		
+		LocalDateTime fromDate = LocalDateTime.now().with(firstDayOfYear());
+		LocalDateTime toDate = LocalDateTime.now().with(lastDayOfYear());
+		
+		List<Measure> measures = new ArrayList<>();
+		measures.add(measureDao.getMax(sensor, fromDate, toDate));
+		measures.add(measureDao.getMin(sensor, fromDate, toDate));
+		
+		if(measures.size() != 2)
+			return Response.status(Response.Status.NOT_FOUND).entity(ErrorServices.OBJECT_NOT_FOUND.getMessage() + " - measures").build();
+		
+		return Response.status(200).entity(measuresToMeasureDtos(measures)).build();
+	}
+	@GET
+	@Path("/sensor/{id}/getValueOfAll")
+	@Produces(MediaType.APPLICATION_JSON)
+	@Transactional
+	public Response getValueOfAll(@PathParam("id") Long id) {
+		Sensor sensor = sensorDao.findById(id);
+		
+		if(sensor == null)
+			return Response.status(Response.Status.NOT_FOUND).entity(ErrorServices.OBJECT_NOT_FOUND.getMessage() + " - sensor").build();
+		
+		
+		LocalDateTime fromDate = LocalDateTime.of(1970, 01, 01, 0, 0);
+		LocalDateTime toDate = LocalDateTime.now().with(lastDayOfYear());
+		
+		List<Measure> measures = new ArrayList<>();
+		measures.add(measureDao.getMax(sensor, fromDate, toDate));
+		measures.add(measureDao.getMin(sensor, fromDate, toDate));
+		
+		if(measures.size() != 2)
+			return Response.status(Response.Status.NOT_FOUND).entity(ErrorServices.OBJECT_NOT_FOUND.getMessage() + " - measures").build();
+		
+		return Response.status(200).entity(measuresToMeasureDtos(measures)).build();
+	}
+	@GET
+	@Path("/sensor/{id}/getValueOfMonth")
+	@Produces(MediaType.APPLICATION_JSON)
+	@Transactional
+	public Response getValueOfMonth(@PathParam("id") Long id) {
+		Sensor sensor = sensorDao.findById(id);
+		
+		if(sensor == null)
+			return Response.status(Response.Status.NOT_FOUND).entity(ErrorServices.OBJECT_NOT_FOUND.getMessage() + " - sensor").build();
+		
+		
+		LocalDateTime fromDate = LocalDateTime.now().with(firstDayOfMonth());
+		LocalDateTime toDate = LocalDateTime.now().with(lastDayOfMonth());
+		
+		List<Measure> measures = new ArrayList<>();
+		measures.add(measureDao.getMax(sensor, fromDate, toDate));
+		measures.add(measureDao.getMin(sensor, fromDate, toDate));
+		
+		if(measures.size() != 2)
+			return Response.status(Response.Status.NOT_FOUND).entity(ErrorServices.OBJECT_NOT_FOUND.getMessage() + " - measures").build();
+		
+		return Response.status(200).entity(measuresToMeasureDtos(measures)).build();
+	}
+	
+	
+	
+	private MeasureDto measureToMeasureDto(Measure measure) {
+		if(measure == null)
+			return null;
+		
+		return MeasureDto.builder()
+				.dateTime(measure.getLocalDateTime().toString())
+				.quantity(StringUtils.floatToString(measure.getQuantity()))
+				.name(measure.getUnitMeasure().getName())
+				.symbol(measure.getUnitMeasure().getSymbol())
+				.build();
+	}
+	
+	private List<MeasureDto> measuresToMeasureDtos(List<Measure> measures) {
+		List<MeasureDto> measureDtos = new ArrayList<>();
+		for (Measure measure : measures) {
+			measureDtos.add(measureToMeasureDto(measure));
 		}
-		
-		String groupby = getGroupBy(fromDate, toDate);
-		
-		List<MeasureChartDto> measures = measureDao.getLotOfMeasureDtoBetweenDate(sensor, 
-				Instant.ofEpochMilli(fromDate).atZone(ZoneId.systemDefault()).toLocalDateTime(), 
-				Instant.ofEpochMilli(toDate).atZone(ZoneId.systemDefault()).toLocalDateTime(),
-				groupby);
-		
-		return Response.status(200).entity(measures).build();
+		return measureDtos;
 	}
-
-
-	private String getGroupBy(Long fromDate, Long toDate) {
-		Long dif = toDate - fromDate;
-		if(dif > GroupByClass.MAX_DAY)
-			return GroupByClass.DAY_GROUPBY;
-		else if(dif > GroupByClass.MAX_WEEK)
-			return GroupByClass.WEEK_GROUPBY;
-		else if(dif > GroupByClass.MAX_MONTH)
-			return GroupByClass.MONTH_GROUPBY;
-		else if(dif > GroupByClass.MAX_YEAR)
-			return GroupByClass.YEAR_GROUPBY;
-		else
-			return GroupByClass.HOUR_GROUPBY;
-		
-	}
+	
+//	@GET
+//	@Path("/lastMeasure")
+//	@Produces(MediaType.APPLICATION_JSON)
+//	@Transactional
+//	public Response getLastOfWeather(@HeaderParam("weatherId") Long weatherId) {
+//		if (weatherId == null)
+//			return Response.status(Response.Status.INTERNAL_SERVER_ERROR).entity(ErrorServices.NULL_OBJECT.getMessage() + " - sensorId").build();
+//
+//		WeatherStation weatherStation = weatherStationDao.fetchById(weatherId);
+//
+//		if(weatherStation == null)
+//			return Response.status(Response.Status.NOT_FOUND).entity(ErrorServices.OBJECT_NOT_FOUND.getMessage() + " - weatherStation").build();
+//		
+//		if(weatherStation.getSensors() == null || weatherStation.getSensors().size() == 0)
+//			return Response.status(Response.Status.NOT_FOUND).entity(ErrorServices.OBJECT_NOT_FOUND.getMessage() + " - sensor").build();
+//
+//		Set<Measure> measures = new HashSet<>();
+//		
+//		for (Sensor sensor : weatherStation.getSensors()) {
+//			measures.add(measureDao.getLastMeasue(sensor));
+//		}
+//	
+//	LocalDateTime fromDate = LocalDateTime.now().toLocalDate().atTime(LocalTime.of(0, 0, 1));
+//	LocalDateTime toDate = LocalDateTime.now().toLocalDate().atTime(LocalTime.of(23, 59, 59));
+//
+//		return Response.status(200).entity(measures).build();
+//	}
+	
+	
+	
+//	@GET
+//	@Path("/getMeasureByDate")
+//	@Produces(MediaType.APPLICATION_JSON)
+//	@Transactional
+//	public Response getMeasureByDate(@HeaderParam("sensorId") Long sensorId, @HeaderParam("fromDate") Long fromDate, @HeaderParam("toDate") Long toDate) {
+//		if (sensorId == null)
+//			return Response.status(Response.Status.INTERNAL_SERVER_ERROR).entity(ErrorServices.NULL_OBJECT.getMessage() + " - sensorId").build();
+//		if (fromDate == null)
+//			return Response.status(Response.Status.INTERNAL_SERVER_ERROR).entity(ErrorServices.NULL_OBJECT.getMessage() + " - fromDate").build();
+//		if (toDate == null)
+//			return Response.status(Response.Status.INTERNAL_SERVER_ERROR).entity(ErrorServices.NULL_OBJECT.getMessage() + " - toDate").build();
+//		
+//		Sensor sensor = sensorDao.findById(sensorId);
+//		
+//		if(sensor == null)
+//			return Response.status(Response.Status.INTERNAL_SERVER_ERROR).entity(ErrorServices.NULL_OBJECT.getMessage() + " - sensor").build();
+//
+//		if(fromDate > toDate) {
+//			Long temp = fromDate;
+//			fromDate = toDate;
+//			toDate = temp;
+//		}
+//		
+//		String groupby = getGroupBy(fromDate, toDate);
+//		
+//		List<MeasureChartDto> measures = measureDao.getLotOfMeasureDtoBetweenDate(sensor, 
+//				Instant.ofEpochMilli(fromDate).atZone(ZoneId.systemDefault()).toLocalDateTime(), 
+//				Instant.ofEpochMilli(toDate).atZone(ZoneId.systemDefault()).toLocalDateTime(),
+//				groupby);
+//		
+//		return Response.status(200).entity(measures).build();
+//	}
+//
+//
+//	private String getGroupBy(Long fromDate, Long toDate) {
+//		Long dif = toDate - fromDate;
+//		if(dif > GroupByClass.MAX_DAY)
+//			return GroupByClass.DAY_GROUPBY;
+//		else if(dif > GroupByClass.MAX_WEEK)
+//			return GroupByClass.WEEK_GROUPBY;
+//		else if(dif > GroupByClass.MAX_MONTH)
+//			return GroupByClass.MONTH_GROUPBY;
+//		else if(dif > GroupByClass.MAX_YEAR)
+//			return GroupByClass.YEAR_GROUPBY;
+//		else
+//			return GroupByClass.HOUR_GROUPBY;
+//		
+//	}
 
 }
 
